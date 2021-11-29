21ac6e9d433a3b9e6fb9698aafe3ba4498a8531c
Gokhan <gokhankabar@hotmail.fr>
2021-11-29 15:00:37 +0100

    joueur 2 bombe explosion colision

--- js/prg.js
+++ js/prg.js
@@ -29,18 +29,34 @@ let posY = 0;
 let posX2=1000;
 let posY2=1000;
 
-let carreposy = 140;
-let carrepox = 140;
+let bombY1 = 0;
+let bombX1 = 0;
+let bombY2 = 0;
+let bombX2 = 0;
+let bomb2 = false;
+
 let zonetab = [];
+let zonetab2 = [];
 
 let all_img2 = [];
 let img2 = new Image();
 img2.src = 'assets/smallerbomb.png';
 let anim_id2 = -1;
 
+let img3 = new Image();
+img3.src = 'assets/smallerbomb.png';
+let all_img3 = [];
+let anim_id3 = -1;
+let firetab2 = [];
+
+
 let explosion = new Image();
 explosion.src = 'assets/boom.png';
 let firetab = [];
+let score1=document.getElementById('s1');
+let score2=document.getElementById('s2');
+let s1=0;
+let s2=0;
 
 img2.onload = function () {
 	let canvas2 = document.createElement('canvas');
@@ -59,6 +75,25 @@ img2.onload = function () {
 		all_img2.push(canvas3);
 	}
 	anim_id2 = 0;
+};
+
+img3.onload = function () {
+	let canvas3 = document.createElement('canvas');
+	canvas3.width = 26 * 6;
+	canvas3.height = 32 * 1;
+	let context2 = canvas3.getContext('2d');
+	context2.drawImage(img3, 0, 0, canvas3.width, canvas3.height);
+	let imax = 6;
+	for (let i = 0; i < imax; i += 1) {
+		let canvasImageData2 = context2.getImageData(i * 26, 0, 26, 32);
+		let canvas4 = document.createElement('canvas');
+		canvas4.width = 26;
+		canvas4.height = 32;
+		let context3 = canvas4.getContext('2d');
+		context3.putImageData(canvasImageData2, 0, 0);
+		all_img3.push(canvas4);
+	}
+	anim_id3 = 0;
 };
 
 
@@ -87,14 +122,14 @@ function update() {
 			if (anim_id2 >= 0) {
 				ctx.drawImage(
 					all_img2[anim_id2],
-					carrepox,
-					carreposy,
+					bombX1,
+					bombY1,
 					26 * zoom2,
 					35 * zoom2
 				);
 				anim_id2 += 1;
 				if (anim_id2 == all_img2.length) {
-					collision_bombe();
+					collision_bombe1();
 					console.log(firetab);
 					anim_id2 = 5;
 						for (let i = 0; i < firetab.length; i++) {
@@ -116,36 +151,80 @@ function update() {
 				}
 			}
 		}
+		for (let i = 0; i < zonetab2.length; i++) {
+			ctx.drawImage(
+				zone,
+				zonetab2[i][0],
+				zonetab2[i][1],
+				zonetab2[i][2],
+				zonetab2[i][3]
+			);
+		}
+		if (bomb2 == true) {
+
+			if (anim_id3 >= 0) {
+				ctx.drawImage(
+					all_img3[anim_id3],
+					bombX2,
+					bombY2,
+					26 * zoom2,
+					35 * zoom2
+				);
+				anim_id3 += 1;
+				if (anim_id3 == all_img3.length) {
+					collision_bombe2();
+					anim_id3 = 5;
+						for (let i = 0; i < firetab2.length; i++) {
+							ctx.drawImage(
+								explosion,
+								firetab2[i][0],
+								firetab2[i][1],
+								123 * zoom3,
+								121 * zoom3
+							);
+						
+
+							
+						}
+					
+					firetab2 = [];
+					anim_id3 = 0;
+					bomb2 = false;
+				}
+			}
+		}
 		ctx.drawImage(all_img[numero], posX, posY, 51 * zoom, 61 * zoom);
 		ctx.drawImage(tabperso2[numero2], posX2, posY2, 51 * zoom, 61 * zoom);
 		ctx.closePath();
 	}
 }
 
-function collision_bombe() {
-	let zonex1 = carrepox + 150;
-	let zoney1 = carreposy + 150;
-	let zonex2 = carrepox - 150;
-	let zoney2 = carreposy - 150;
+function collision_bombe1() {
+	let zonex1 = bombX1 + 150;
+	let zoney1 = bombY1 + 150;
+	let zonex2 = bombX1 - 150;
+	let zoney2 = bombY1 - 150;
 	for (let i = 0; i < posBuisson.length; i++) {
 		if (
 			(zonex1 < posBuisson[i][0] + posBuisson[i][2] &&
 				zonex1 + 50 > posBuisson[i][0] &&
-				carreposy < posBuisson[i][1] + posBuisson[i][3] &&
-				50 + carreposy > posBuisson[i][1]) ||
+				bombY1 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + bombY1 > posBuisson[i][1]) ||
 			(zonex2 < posBuisson[i][0] + posBuisson[i][2] &&
 				zonex2 + 50 > posBuisson[i][0] &&
-				carreposy < posBuisson[i][1] + posBuisson[i][3] &&
-				50 + carreposy > posBuisson[i][1]) ||
-			(carrepox < posBuisson[i][0] + posBuisson[i][2] &&
-				carrepox + 50 > posBuisson[i][0] &&
+				bombY1 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + bombY1 > posBuisson[i][1]) ||
+			(bombX1 < posBuisson[i][0] + posBuisson[i][2] &&
+				bombX1 + 50 > posBuisson[i][0] &&
 				zoney2 < posBuisson[i][1] + posBuisson[i][3] &&
 				50 + zoney2 > posBuisson[i][1]) ||
-			(carrepox < posBuisson[i][0] + posBuisson[i][2] &&
-				carrepox + 50 > posBuisson[i][0] &&
+			(bombX1 < posBuisson[i][0] + posBuisson[i][2] &&
+				bombX1 + 50 > posBuisson[i][0] &&
 				zoney1 < posBuisson[i][1] + posBuisson[i][3] &&
 				50 + zoney1 > posBuisson[i][1])
 		) {
+			s1+=100;
+			score1.textContent= s1;
 			zonebool = true;
 			zonetab.push([
 				posBuisson[i][0],
@@ -154,6 +233,49 @@ function collision_bombe() {
 				posBuisson[i][3],
 			]);
 			firetab.push([
+				posBuisson[i][0],
+				posBuisson[i][1],
+				posBuisson[i][2],
+				posBuisson[i][3],
+			]);
+			posBuisson.splice(i, 1);
+		}
+	}
+}
+function collision_bombe2() {
+	let zonex1 = bombX2 + 150;
+	let zoney1 = bombY2 + 150;
+	let zonex2 = bombX2 - 150;
+	let zoney2 = bombY2 - 150;
+	for (let i = 0; i < posBuisson.length; i++) {
+		if (
+			(zonex1 < posBuisson[i][0] + posBuisson[i][2] &&
+				zonex1 + 50 > posBuisson[i][0] &&
+				bombY2 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + bombY2 > posBuisson[i][1]) ||
+			(zonex2 < posBuisson[i][0] + posBuisson[i][2] &&
+				zonex2 + 50 > posBuisson[i][0] &&
+				bombY2 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + bombY2 > posBuisson[i][1]) ||
+			(bombX2 < posBuisson[i][0] + posBuisson[i][2] &&
+				bombX2 + 50 > posBuisson[i][0] &&
+				zoney2 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + zoney2 > posBuisson[i][1]) ||
+			(bombX2 < posBuisson[i][0] + posBuisson[i][2] &&
+				bombX2 + 50 > posBuisson[i][0] &&
+				zoney1 < posBuisson[i][1] + posBuisson[i][3] &&
+				50 + zoney1 > posBuisson[i][1])
+		) {
+			s2+=100;
+			score2.textContent= s2;
+			zonebool = true;
+			zonetab2.push([
+				posBuisson[i][0],
+				posBuisson[i][1],
+				posBuisson[i][2],
+				posBuisson[i][3],
+			]);
+			firetab2.push([
 				posBuisson[i][0],
 				posBuisson[i][1],
 				posBuisson[i][2],
@@ -247,8 +369,8 @@ function keydown_fun(e) {
 		case 'Space':
 			if (bomb == false) {
 				bomb = true;
-				carreposy = posY;
-				carrepox = posX;
+				bombY1 = posY;
+				bombX1 = posX;
 			}
 
 			break;
@@ -282,11 +404,11 @@ function keydown_fun(e) {
 				collision2(0, +20);
 				break;
 	
-		case 'g':
-				if (bomb == false) {
-					bomb = true;
-					carreposy = posY2;
-					carrepox = posX2;
+		case 'Shift':
+				if (bomb2 == false) {
+					bomb2 = true;
+					bombY2 = posY2;
+					bombX2 = posX2;
 				}
 	
 				break;
